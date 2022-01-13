4334a413b6b63114fe65c3d72084526460f83745
unknown <aniketghosh810@gmail.com>
2022-01-13 11:23:23 +0530

    Added workin list tah  endpoint.

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from django.utils.translation import gettext as _
 
+
 from core import models
 
 
@@ -29,4 +30,5 @@ class UserAdmin(BaseUserAdmin):
     )
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Tag)
 
